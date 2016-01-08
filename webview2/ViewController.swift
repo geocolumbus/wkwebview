da5178f1dd07918285d760373754804743040be1
George Campbell <geocolumbus@gmail.com>
2016-01-08 15:59:57 -0500

    Added wkwebview sample files.

--- webview2/ViewController.swift
+++ webview2/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView
+    
+    required init(coder aDecoder: NSCoder) {
+        self.webView = WKWebView(frame: CGRectZero)
+        super.init(coder: aDecoder)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(webView)
+        //webView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        webView.translatesAutoresizingMaskIntoConstraints = false;
+        let height = NSLayoutConstraint(item: webView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0)
+        let width = NSLayoutConstraint(item: webView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0)
+        view.addConstraints([height, width])
+        let url = NSURL(string:"http://www.google.com")
+        let request = NSURLRequest(URL:url!)
+        webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
